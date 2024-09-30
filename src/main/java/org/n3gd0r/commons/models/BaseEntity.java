4aa0c2e21e6c908bf45f2e7fa7707090f265179e
N3gD0r <70918021+N3gD0r@users.noreply.github.com>
2024-09-30 10:45:56 -0600

    Added persistence inheritance strategies

--- src/main/java/org/n3gd0r/commons/models/BaseEntity.java
+++ src/main/java/org/n3gd0r/commons/models/BaseEntity.java
@@ -1,13 +1,17 @@
 package org.n3gd0r.commons.models;
 
+import static jakarta.persistence.InheritanceType.TABLE_PER_CLASS;
+
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Inheritance;
 import lombok.Data;
 
 /**
  * BaseEntity
  */
 @Data
+@Inheritance(strategy = TABLE_PER_CLASS)
 public abstract class BaseEntity<TKey> {
 	private TKey id;
 	private Boolean status;
--- src/main/java/org/n3gd0r/commons/models/CommonEntity.java
+++ src/main/java/org/n3gd0r/commons/models/CommonEntity.java
@@ -1,7 +1,10 @@
 package org.n3gd0r.commons.models;
 
+import static jakarta.persistence.InheritanceType.TABLE_PER_CLASS;
+
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Inheritance;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -10,6 +13,7 @@ import lombok.EqualsAndHashCode;
  */
 @EqualsAndHashCode(callSuper = true)
 @Data
+@Inheritance(strategy = TABLE_PER_CLASS)
 public abstract class CommonEntity<TKey> extends BaseEntity<TKey> {
 	private TKey userId;
 	private LocalDateTime updatedAt;
